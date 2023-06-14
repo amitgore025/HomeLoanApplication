18c4d25d5b5b4bbd1aa6025ec0c22f85d5871c6a
amitgore025 <amitgore025@gmail.com>
2023-06-14 22:53:22 +0530

    initial

---
+++ src/test/java/org/hla/main/HomeLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hla.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
