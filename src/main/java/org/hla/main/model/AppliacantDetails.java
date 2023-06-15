ee97064f7b90ac885c4f9d576a69b81c25287eea
AmolBhusari123 <amol.bhusari888@gmail.com>
2023-06-15 10:32:42 +0530

    Amol

--- src/main/java/org/hla/main/model/AppliacantDetails.java
+++ src/main/java/org/hla/main/model/AppliacantDetails.java
@@ -7,5 +7,14 @@ public class AppliacantDetails {
 
 	private String applicantName;
 	private String applicantCity;
+	
+	private String customerFirstName;
+	private String customerMiddleName;
+	private String customerLastName;
+	private Long customerMobileNumber;
+	private String customerDateOfBirth;
+	private String customerEmailid;
+	private String customerGender;
+	private String customerQualification;
 
 }
